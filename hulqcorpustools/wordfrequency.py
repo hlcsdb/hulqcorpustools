9617b58bdae978edaea2ba8eee96eeeb9b8dce81
zack <zack.gilkison@gmail.com>
2022-10-14 15:03:55 -0700

    cute word template - should be helpful

--- hulqcorpustools/wordfrequency.py
+++ hulqcorpustools/wordfrequency.py
@@ -1,34 +1,18 @@
+
 from collections import Counter
-from flashtext import KeywordProcessor as kp
 import json
-import pandas as pd
 from pathlib import Path
 import regex as re
+from typing import Optional
 
-import hwordlists as hw
+from flashtext import KeywordProcessor as kp
+import pandas as pd
 
-# paths
-text_corpus = Path(__file__).resolve().parent / 'corpus' /  'Texts-corpus-ZG8.txt'
-word_frequency_wordlist_path = Path(Path(__file__).resolve().parent / 'hulqwordfrequency/output/word_frequency_wordlist_incl.json')
+import rawtextfixer
+from hulqcorpusresources.wordlists import hulq_wordlist
 
-# regular expression patterns
-hulqletters = re.compile(r'a-zA-Z’')
-rl = re.compile('([aeiou][\.|\-]){2,}')
-strip_punctuation = re.compile('[\.|,|?|“|"|”|\[|\]|1|2|3|4|5|6|7|8|9|0|\(|\)]')
-is_number = re.compile('[0-9]+')
 
-def strip_rl_and_punc(word: str) -> str:
-    '''strips off rhetorical lengthening (if any) and extraneous punctuation'''
-
-    word_sub = lambda x: strip_punctuation.sub('', rl.sub(r'\1', x))
-    
-    if len(word_sub(word)) == 0:
-        return(word)
-
-    
-    return(word_sub(word))
-
-def count_corpus(open_file: Path, **kwargs) -> Counter:
+def count_corpus(text_corpus_path: Path, **kwargs) -> Counter:
     """counts the incidence of each word in the corpus
 
     Arguments:
@@ -47,13 +31,13 @@ def count_corpus(open_file: Path, **kwargs) -> Counter:
         wordlist_keywordprocessor.add_non_word_boundary('’')
         # for now: the wordlist is just the hp one
         wordlist_keywordprocessor.add_keywords_from_list(
-            [i.strip() for i in hukari_peter_wordlist_from_df()]
+            hulq_wordlist
             )
     else:
         wordlist_flag = False
         wordlist_keywordprocessor = []
     
-    with open(text_corpus, 'r') as open_file:
+    with open(text_corpus_path, 'r') as open_file:
         words = Counter()
         # counting a couple for testing purposes
         line_limit = kwargs.get('line_limit')
@@ -65,11 +49,11 @@ def count_corpus(open_file: Path, **kwargs) -> Counter:
             if line[0:2] == 'LH':
                 line_list = line.split()
 
-                line_list = [strip_rl_and_punc(word) for word in line.split()]
+                line_list = [rawtextfixer.strip_rl_and_punc(word) for word in line.split()]
                 # skip 'LH' and line number
                 for word in line_list:
                     if word == 'LH' or \
-                    is_number.fullmatch(word) or \
+                    rawtextfixer.is_number.fullmatch(word) or \
                     (wordlist_flag == True and word not in wordlist_keywordprocessor):
                         continue
                     elif words.get(word):
@@ -105,10 +89,6 @@ def write_frequency_to_json(output_filepath: Path, counted_words: Counter):
     with open(output_filepath, 'w+') as open_file:
         json.dump(dict(counted_words.most_common()), open_file, ensure_ascii=False, indent=0)
 
-def hukari_peter_wordlist_from_df():
-    """just gets the wordlist from the df"""
-    return hw.HulqWordlists.hukari_peter_xlsx_df.get('LEXEME - curly ’unuhw')
-
 def add_wordlist_to_frequency_list(
     corpus_count: Counter,
     wordlist: pd.DataFrame) -> Counter:
@@ -125,18 +105,24 @@ def add_wordlist_to_frequency_list(
     """
     corpus_count.update((i.strip() for i in wordlist))
 
-def build_frequency_json(text_corpus_path: Path, word_frequency_wordlist_path: Path):
+def build_frequency_json(
+    text_corpus_path: Path,
+    word_frequency_wordlist_path: Path,
+    wordlist: Optional[Path]):
     """builds the frequency json
 
     Arguments:
         text_corpus -- path to text corpus
     """
     corpus_count = count_corpus(text_corpus_path, wordlist=True)
-    add_wordlist_to_frequency_list(corpus_count, hukari_peter_wordlist_from_df())
+    add_wordlist_to_frequency_list(corpus_count, wordlist)
 
     #json output assumed to be in the same place as the wordlist
     json_output_path = word_frequency_wordlist_path
     write_frequency_to_json(corpus_count, json_output_path)
 
 if __name__ == "__main__":
-    build_frequency_json(text_corpus, word_frequency_wordlist_path)
+    corpus_data_path = Path(__file__).resolve().parent / 'corpusdata'
+    word_frequency_wordlist_data_path = corpus_data_path / 'word-frequency.json'
+    text_corpus_path = list(corpus_data_path.glob('*corpus*.txt'))[0]
+    build_frequency_json(text_corpus_path, word_frequency_wordlist_data_path, hulq_wordlist)
