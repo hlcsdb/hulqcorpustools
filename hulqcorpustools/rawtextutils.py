98aa3bad3cb7cefe9f21f5e57b8054bebc235e03
zack <zack.gilkison@gmail.com>
2022-10-19 15:13:49 -0700

    adding new raw text fns
    move spellchecker from somewhere else

--- hulqcorpustools/rawtextutils.py
+++ hulqcorpustools/rawtextutils.py
@@ -1,4 +1,12 @@
 
+from pathlib import Path
+from typing import Optional
+
+from flashtext import KeywordProcessor as kp
+
+from hulqcorpusresources.constants import FileFormat
+from hulqcorpusresources.wordlists import Wordlist
+
 import re
 
 hulqletters = re.compile(r'a-zA-Z’')
@@ -14,5 +22,50 @@ def strip_rl_and_punc(word: str) -> str:
     if len(word_sub(word)) == 0:
         return(word)
 
-    
     return(word_sub(word))
+
+def _load_flashtext(
+    _wordlist_format: str | None
+    ):
+
+    
+    if _wordlist_format is None:
+        _wordlist = Wordlist.ORTHOGRAPHY_WORDLIST_TEXT
+
+    else:
+        _wordlist = Wordlist.load_wordlist_text(_wordlist_format)
+
+    _wordlist = _wordlist.split()
+
+    _hulq_kp = kp()
+    _hulq_kp.add_non_word_boundary('’')
+    _hulq_kp.add_keywords_from_list(
+        _wordlist
+            )
+
+    return _hulq_kp
+
+
+def _determine_language_for_line(
+    linetext: str,
+    hulq_kp: kp,
+    english_kp: kp) -> FileFormat:
+    """determines which language the line is in
+
+    Arguments:
+        linetext -- _description_
+        hulq_kp -- _description_
+        english_kp -- _description_
+
+    Returns:
+        _description_
+    """
+
+
+    hulq_count = 0
+    english_count = 0
+    found_hulq_words = hulq_kp.extract_keywords(linetext)
+    found_english_words = english_kp.extract_keywords(linetext)
+
+    print(found_hulq_words, found_english_words)
+    ...
