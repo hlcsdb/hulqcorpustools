f1435837bd7831d1f41b41568d4fbbe4e36a5bb7
zack <zack.gilkison@gmail.com>
2023-10-12 16:37:29 -0700

    changed folder name but not manifest
    moving around
    
    not needed or causes problems

--- dev_gunicorn.sh
+++ dev_gunicorn.sh
@@ -2,4 +2,4 @@
 
 echo "---\nstarting gunicorn now ...\n"
 # only works if gunicorn is installed a local venv, where that venv is called 'venv'
-./venv/bin/gunicorn --conf './gunicorn_conf.py' 'hulqcorpustools.hulqtransliterator.onlinetransliterator.app:app'
+./venv/bin/gunicorn --conf './gunicorn_conf.py' 'hulqcorpustools.webapps.app:app'
