cea404906dcccc3abedd1322006a52f12b868c8c
zack <zack.gilkison@gmail.com>
2023-11-08 13:30:38 -0800

    init file handler

--- hulqcorpustools/hulqtransliterator/transliterator/controller.py
+++ hulqcorpustools/hulqtransliterator/transliterator/controller.py
@@ -15,6 +15,7 @@ import subprocess
 import importlib.resources
 
 from hulqcorpustools.resources.wordlists import wordlist_paths
+from hulqcorpustools.utils.files import FileHandler
 from hulqcorpustools.utils.keywordprocessors import HulqKeywordProcessors
 from hulqcorpustools.resources.constants import FileFormat, TransliterandFile, GraphemesDict
 from hulqcorpustools.hulqtransliterator.filehandlers import docworker, txtworker
@@ -22,7 +23,7 @@ from . import replaceengine as repl
 
 hulq_kp = HulqKeywordProcessors(eng=True)
 
-class FileController():
+class TransliteratorFileHandler(FileHandler):
     """Class that prepares the KeywordProcessors and transliterates the list of
     given files on demand.
     """
@@ -32,46 +33,12 @@ class FileController():
             source_format = (FileFormat | str | None),
             target_format = (FileFormat | str | None),
             **kwargs):
-        """Prepare KeywordProcessors of the source format and target format 
-        and hold lists of files for transliterating upon request.
-
-        The reason docx files and txt files are both included in one controller
-        is so as to not load multiple KeywordProcessors as it can take
-        a few seconds each time it is initialized. The KPs can be loaded once
-        to transliterate all desired files.
-
         
-        Kwargsuments:
-            docx_files -- a list of Paths to docx files to be transliterated
-            txt_files: a list of Paths to txt files to be transliterated
-            font  in the case of the font search, which font to search by
-        """
-
-
-        self.doc_files = list(filter(lambda x: x.suffix =='.doc', files_list)) # type: list[Path]
+        self.doc_files = list(filter(lambda x: x.suffix =='.doc', files_list)) 
         self.docx_files = list(filter(lambda x: x.suffix == '.docx' and x.stem[0] != "~", files_list))
         self.txt_files = list(filter(lambda x: x.suffix == '.txt', files_list))
         self.out_dir = kwargs.get('outdir')
         self.tmp_dir = kwargs.get('tmpdir')
-
-        if self.doc_files != []:
-            if shutil.which('soffice'):
-                soffice_convert_cmd = ['soffice', '--headless', '--convert-to', 'docx']
-                
-                
-                if self.tmp_dir:
-                    _doc_convert_tmpdir = self.tmp_dir
-                # use first file location if none provided
-                else:
-                    _doc_convert_tmpdir = self.doc_files[0].parent
-                soffice_convert_cmd.extend(['--outdir', _doc_convert_tmpdir])
-                soffice_convert_cmd.extend((str(_doc_file) for _doc_file in self.doc_files))
-                subprocess.run(soffice_convert_cmd)
-                _new_docx_files = [_doc_convert_tmpdir.joinpath(_doc_file.with_suffix('.docx')) for _doc_file in self.doc_files]
-
-                self.docx_files.extend(_new_docx_files)
-            else:
-                print('libreoffice not installed! Skipping .doc files...')
 
         self.source_format = source_format
         self.target_format = target_format
---
+++ hulqcorpustools/utils/files.py
@@ -0,0 +1,44 @@
+
+from pathlib import Path
+import shutil
+import subprocess
+
+from docx import Document as load_docx
+from docx.document import Document
+
+from hulqcorpustools.resources.constants import FileFormat
+
+class FileHandler():
+
+    def __init__(
+            self,
+            files_list = (list[Path]),
+
+            **kwargs):
+
+
+        self.doc_files = list(filter(lambda x: x.suffix =='.doc', files_list)) 
+        self.docx_files = list(filter(lambda x: x.suffix == '.docx' and x.stem[0] != "~", files_list))
+        self.txt_files = list(filter(lambda x: x.suffix == '.txt', files_list))
+        self.out_dir = kwargs.get('outdir')
+        self.tmp_dir = kwargs.get('tmpdir')
+
+        if self.doc_files != []:
+            if shutil.which('soffice'):
+                soffice_convert_cmd = ['soffice', '--headless', '--convert-to', 'docx']
+                
+                
+                if self.tmp_dir:
+                    _doc_convert_tmpdir = self.tmp_dir
+                # use first file location if none provided
+                else:
+                    _doc_convert_tmpdir = self.doc_files[0].parent
+                soffice_convert_cmd.extend(['--outdir', _doc_convert_tmpdir])
+                soffice_convert_cmd.extend((str(_doc_file) for _doc_file in self.doc_files))
+                subprocess.run(soffice_convert_cmd)
+                _new_docx_files = [_doc_convert_tmpdir.joinpath(_doc_file.with_suffix('.docx')) for _doc_file in self.doc_files]
+
+                self.docx_files.extend(_new_docx_files)
+            else:
+                print('libreoffice not installed! Skipping .doc files...')
+
